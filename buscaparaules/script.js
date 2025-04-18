459fb8d2fe947fdb1d1564d591e57714dd3c74b1
adriatp <tarradas.adria@gmail.com>
2025-04-18 13:14:59 +0200

    add direct json file with parsed dict

--- buscaparaules/script.js
+++ buscaparaules/script.js
@@ -5,25 +5,50 @@ function create_dictionary(url) {
 	fetch(url)
     .then(response => response.text())
     .then(dict => {
-        words = dict.split(/\r?\n/).map(p => p.trim()).filter(p => p.length);
-		for (let word of words) {
-			const sorted_word = sort_word(word);
-			if (!dictionary.has(sorted_word)){
-				dictionary.set(sorted_word, []);
+			words = dict.split(/\r?\n/).map(p => p.trim()).filter(p => p.length);
+			for (let word of words) {
+				const sorted_word = sort_word(word);
+				if (!dictionary.has(sorted_word)){
+					dictionary.set(sorted_word, []);
+				}
+				dictionary.get(sorted_word).push(word);
 			}
-			dictionary.get(sorted_word).push(word);
-		}
-		console.log("ready");
+			console.log("ready");
+			// download_object_as_json(dictionary);
     })
     .catch(error => {
         console.error("Error:", error);
     });
 }
 
+function download_object_as_json(obj) {
+	const json = JSON.stringify(Object.fromEntries(obj));
+	console.log(obj)
+	console.log(json)
+	const blob = new Blob([json], { type: 'application/json' });
+	const url = URL.createObjectURL(blob);
+	const a = document.createElement('a');
+	a.href = url;
+	a.download = 'data.json';
+	a.style.display = 'none';
+	document.body.appendChild(a);
+	a.click();
+	document.body.removeChild(a);
+	URL.revokeObjectURL(url);
+}
+
 function find_words(){
 	let input_letters = document.querySelector("#input_letters");
 	let sorted_letters = sort_word(input_letters.value);
-	alert(dictionary.get(sorted_letters));
+	const ul = document.querySelector('#found_words');
+	ul.textContent = '';
+	for (let found_word of dictionary.get(sorted_letters)) {
+		const li = document.createElement('li');
+		li.className = 'list-group-item';
+		li.textContent = found_word;
+		ul.appendChild(li);
+	}
+	// alert(dictionary.get(sorted_letters));
 }
 
 function sort_word(word){
